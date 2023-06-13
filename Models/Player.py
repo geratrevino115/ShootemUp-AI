57c58d381ce2effdc9f9628b51963c5e65f34214
Antonio Paniagua <tony.paniagua7@hotmail.com>
2023-06-13 00:05:46 -0600

    Create Player.py
    add icon and move

---
+++ Models/Player.py
@@ -0,0 +1,39 @@
+import pygame
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("assets/rocket.png")
+        self.image = pygame.transform.scale(self.image, (62, 108))
+        self.rect = self.image.get_rect()
+        self.rect.center = (320, 200)
+
+        self.vel = pygame.math.Vector2(0, 0)
+        self.acc = pygame.math.Vector2(0, 0)
+
+    def move(self, ACC, FRIC):
+        self.acc = pygame.math.Vector2(0, 0)
+        pressed_keys = pygame.key.get_pressed()
+
+        if pressed_keys[pygame.K_LEFT]:
+            self.acc.x = -1
+        if pressed_keys[pygame.K_RIGHT]:
+            self.acc.x = 1
+        if pressed_keys[pygame.K_UP]:
+            self.acc.y = -1
+        if pressed_keys[pygame.K_DOWN]:
+            self.acc.y = 1
+
+        self.acc.x += self.vel.x * FRIC
+        self.acc.y += self.vel.y * FRIC
+        self.vel += self.acc
+        self.rect = self.rect.move(self.vel)
+
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > 640:
+            self.rect.right = 640
+        if self.rect.top < 0:
+            self.rect.top = 0
+        if self.rect.bottom > 400:
+            self.rect.bottom = 400
